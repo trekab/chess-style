9cc8fe4704ee965730e9134cbe890289226e02f3
Treasure Kabareebe <treasurecalm@gmail.com>
2020-04-01 01:46:39 +0300

    Add login and logout functionality

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,25 @@
+class SessionsController < ApplicationController
+	
+	def new
+			
+	end
+
+	def create
+		user = User.find_by(name: params[:session][:name])
+		if user
+			session[:user_id] = user.id
+			#flash[:success] = "You have successfully logged in"
+			redirect_to pages_home_path
+		else
+			#flash.now[:danger] = "There was something wrong with your login information"
+			render 'new'
+		end
+	end
+	
+	def destroy
+		session[:user_id] = nil
+		# flash[:success] = "You have successfully logged out"
+		redirect_to login_path
+	end
+	
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,7 @@ Rails.application.routes.draw do
   get 'pages/home'
   get 'sign-up', to: 'users#new'
   post 'sign-up', to: 'users#create'
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
 end
