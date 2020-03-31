d2a499c77ec1653258e46aa04e94f6089c99bbb6
Treasure Kabareebe <treasurecalm@gmail.com>
2020-04-01 01:08:54 +0300

    Add sign up capability

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'pages/home'
+  get 'sign-up', to: 'users#new'
+  post 'sign-up', to: 'users#create'
 end
