007be0372bb3cc98ef0da2e3f399c8feeb3cbdbb
Treasure Kabareebe <treasurecalm@gmail.com>
2020-03-31 20:19:40 +0300

    Add articles model

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_31_170119) do
+ActiveRecord::Schema.define(version: 2020_03_31_170916) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "articles", force: :cascade do |t|
+    t.string "title"
+    t.text "text"
+    t.text "image"
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_articles_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
@@ -21,4 +31,5 @@ ActiveRecord::Schema.define(version: 2020_03_31_170119) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "articles", "users"
 end
