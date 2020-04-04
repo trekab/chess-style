3b4e758129ce93cbf278a94e585051bc92a9c9f0
Treasure Kabareebe <treasurecalm@gmail.com>
2020-04-04 07:54:17 +0300

    Add categories show page

--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,5 @@ Rails.application.routes.draw do
   get 'login', to: 'sessions#new'
   post 'login', to: 'sessions#create'
   delete 'logout', to: 'sessions#destroy'
+  get 'category/:id', to: 'categories#show', as: 'category'
 end
