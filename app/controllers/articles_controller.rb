8b75ba784fcef72e240afc52fcfdfbd3e34d33c4
Treasure Kabareebe <treasurecalm@gmail.com>
2020-04-04 21:43:24 +0300

    Add form for creating a new article

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -3,4 +3,8 @@ class ArticlesController < ApplicationController
   	@featured_article = Article.featured_article
   	@categories = Category.all.order(:priority).reverse_order
   end
+
+  def new
+  	@article = Article.new
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,5 @@ Rails.application.routes.draw do
   post 'login', to: 'sessions#create'
   delete 'logout', to: 'sessions#destroy'
   get 'category/:id', to: 'categories#show', as: 'category'
+  get 'create-article', to: 'articles#new'
 end
