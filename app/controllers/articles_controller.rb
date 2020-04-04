5bf295b7ecdfeed6d13af175fb9c40cbb6a5b999
Treasure Kabareebe <treasurecalm@gmail.com>
2020-04-04 07:19:36 +0300

    Add logic for listing categories in order of priority on the home page

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,6 @@
 class ArticlesController < ApplicationController
   def index
   	@featured_article = Article.featured_article
+  	@categories = Category.all.order(:priority).reverse_order
   end
 end
