2e12ebfbf050cf5503e7db31c362cca47bc14538
Treasure Kabareebe <treasurecalm@gmail.com>
2020-04-07 01:55:23 +0300

    Style root page

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,5 +1,5 @@
 class Article < ApplicationRecord
-  validates_presence_of :title, :text
+  validates_presence_of :title, :text, :image
   validates :title, :text, length: { minimum: 6 }
   belongs_to :author, class_name: 'User', foreign_key: 'user_id'
   has_many :article_categories
